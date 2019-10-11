173aab6065497dfac34fa041c5c71ff08c154863
Leon <zhaojincheng@Leon-MacBook-Pro.local>
2019-10-11 23:05:17 +0800

    2

---
+++ example/test.php
@@ -0,0 +1,6 @@
+
+<?php
+require_once '../vendor/autoload.php';
+ 
+$hello = new Soeasy\Demo\HelloWorld('My First test');
+echo $hello->hello();
---
+++ src/HelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+namespace Soeasy\Demo;
+
+class HelloWorld
+{
+    public function __construct($name = 'Soeasy')
+    {
+        $this->name = $name;
+    }
+    public function hello()
+    {
+        return 'Hello '.$this->name.'!';
+    }
+}
+?>
